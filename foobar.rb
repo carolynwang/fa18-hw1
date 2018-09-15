541e1e9aa6d2319912c2070113a4d10cdfabe099
carolynwang <carolyn.wang@berkeley.edu>
2018-09-15 16:54:18 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,12 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.map{ |el| el.to_i }
+    a = a.map{ |el| el + 2 }
+    a = a.delete_if{ |el| el % 2 != 0 }
+    a = a.delete_if{ |el| el > 10 }
+    sum = 0
+    a.each{ |el| sum += el }
+    sum
   end
 end
